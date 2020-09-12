803d7dc125a69b0d52ac92791df9dec31f50f7dc
EIYA-ASUS <EIYA5498513@noreply.github.com>
2020-09-12 14:32:05 +0900

    break

--- go/main.go
+++ go/main.go
@@ -961,6 +961,10 @@ func searchEstateNazotte(c echo.Context) error {
 		} else {
 			estatesInPolygon = append(estatesInPolygon, validatedEstate)
 		}
+
+		if len(estatesInPolygon) >= NazotteLimit {
+			break
+		}
 	}
 
 	var re EstateSearchResponse
